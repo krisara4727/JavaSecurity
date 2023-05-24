d8aa42e3dda81173d454654b57eb2d9e1356137b
krishna.chivte <krishna.chivte@in.abb.com>
2023-05-24 17:30:11 +0530

    spring security with oAuth2

--- springsecurity/spring-security-client/src/main/java/com/krishna/springsecurityclient/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-//package com.krishna.springsecurityclient.repository;
-//
-//import com.krishna.springsecurityclient.entity.User;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface UserRepository extends JpaRepository<User, Long> {
-//}
